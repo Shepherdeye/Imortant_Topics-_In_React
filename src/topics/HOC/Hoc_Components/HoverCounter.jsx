e293ddcfb65857accf29f63351d937f81debffab
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-02-21 23:22:29 +0200

    adding the homePage

--- src/topics/HOC/Hoc_Components/HoverCounter.jsx
+++ src/topics/HOC/Hoc_Components/HoverCounter.jsx
@@ -4,7 +4,8 @@ export default class HoverCounter extends Component {
     render() {
         return (
             <div>
-                <button className='bg-gray-200 p-3 rounded mb-2 text-blue-600 font-bold cursor-pointer hover:bg-gray-400'>
+                <button className='bg-gray-200 p-3 rounded mb-2 text-blue-600 font-bold cursor-pointer
+                 hover:bg-gray-400'>
                     User Hover {0} times
                 </button>
             </div>
