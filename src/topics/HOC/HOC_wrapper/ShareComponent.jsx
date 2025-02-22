6f2c776ed6451ab6711119a6796abc0bcea46379
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-02-22 02:17:49 +0200

    add the  HOC function

--- src/topics/HOC/HOC_wrapper/ShareComponent.jsx
+++ src/topics/HOC/HOC_wrapper/ShareComponent.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
 
-export default class ShareComponent extends Component {
-    render() {
-        return (
-            <div>ShareComponent</div>
-        )
-    }
+const Counter = (WrappedComponent) => {
+    return (
+        class Counter extends Component {
+            constructor(props) {
+                super(props)
+
+                this.state = {
+                    count: 0
+                }
+            }
+
+            incrementcount = () => {
+                return this.setState({
+                    count: this.state.count + 1
+                })
+            }
+
+            render() {
+                return <WrappedComponent
+                    count={this.state.count}
+                    incrementcount={this.incrementcount} />
+            }
+        }
+
+    )
 }
+
+export default Counter
--- src/topics/HOC/Hoc_Components/HoverCounter.jsx
+++ src/topics/HOC/Hoc_Components/HoverCounter.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
+import Counter from '../HOC_wrapper/ShareComponent'
 
-export default class HoverCounter extends Component {
+class HoverCounter extends Component {
+
     render() {
+        const { count, incrementcount } = this.props
         return (
             <div>
-                <button className='bg-gray-200 p-3 rounded mb-2 text-blue-600 font-bold cursor-pointer
-                 hover:bg-gray-400'>
-                    User Hover {0} times
+                <button
+                    onMouseMove={incrementcount}
+                    className='bg-gray-200 p-3 rounded mb-2 text-blue-600 font-bold cursor-pointer  hover:bg-gray-400'>
+                    User clicked {count} times
                 </button>
             </div>
         )
     }
 }
+export default Counter(HoverCounter)
