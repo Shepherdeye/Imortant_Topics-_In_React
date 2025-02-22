5c596645d7caa0c2cf42fa6928442d89386aea44
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-02-22 02:32:12 +0200

    test

--- src/topics/HOC/Hoc_Components/ClickCounter.jsx
+++ src/topics/HOC/Hoc_Components/ClickCounter.jsx
@@ -1,13 +1,4 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD
-
-export default class ClickCounter extends Component {
-    render() {
-        return (
-            <div>
-                <button className='bg-gray-200 p-3 rounded mb-2 text-blue-600 font-bold cursor-pointer  hover:bg-gray-400'>
-                    User clicked {0} times
-=======
 import Counter from '../HOC_wrapper/ShareComponent'
 
 class ClickCounter extends Component {
@@ -18,16 +9,12 @@ class ClickCounter extends Component {
             <div>
                 <button
                     onClick={incrementcount}
-                    className='bg-gray-200 p-3 rounded mb-2 text-blue-600 font-bold cursor-pointer  hover:bg-gray-400'>
+                    className='bg-gray-300 p-3 rounded mb-2 text-blue-600 font-bold cursor-pointer  hover:bg-gray-400'>
                     User clicked {count} times
->>>>>>> test
                 </button>
             </div>
         )
     }
 }
-<<<<<<< HEAD
-=======
 
 export default Counter(ClickCounter)
->>>>>>> test
