6bdda0815acfbb7251e76affb0650b70fac7dced
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-03-09 22:13:02 +0200

    adding the recording dev

--- src/topics/Echo/Echo.jsx
+++ src/topics/Echo/Echo.jsx
@@ -6,8 +6,29 @@ const Echo = () => {
             <h1 className='text-3xl text-center'>
                 This is the Echo Audio Test
             </h1>
+            {/*this is the recording Dev*/}
+            <div className='flex flex-col items-center justify-center my-8'>
+                <div className=' flex items-center justify-around w-5/10'>
+                    <div className='bg-red-800 p-3 text-xl w-max rounded-sm '>Start Record</div>
+                    <div className='bg-green-800 p-3 text-xl w-max rounded-sm'>Resume Record</div>
+                    <div className='bg-gray-400 p-3 text-xl w-max rounded-sm'>Pause Record</div>
+                    <div className='bg-yellow-800 p-3 text-xl w-max rounded-sm'>Restart Record</div>
+                </div>
 
 
+                {/* recording Dev */}
+                <div className='bg-red-700 text-2xl text-white font-bold'>
+
+                </div>
+
+                {/* recored Or Uploaded Audio */}
+                <audio controls src="" className="my-5" ></audio>
+            </div>
+
+            {/*this is the Result Dev*/}
+            <div>
+
+            </div>
         </div>
     )
 }
