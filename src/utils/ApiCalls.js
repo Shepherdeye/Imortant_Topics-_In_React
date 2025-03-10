1b16241e66b88c162a624e2ba229eeac532dde88
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-03-10 02:16:29 +0200

    making  the logic  of the echo

--- src/utils/ApiCalls.js
+++ src/utils/ApiCalls.js
@@ -1,6 +1,25 @@
-export function SendToKateb() {
+import axios from "axios";
 
-}
-export function SendToNatiq() {
+export const sendToKatib = async (audioFile) => {
+    if (!audioFile) {
+        alert("Fetch Error: there is No Audio Selected");
+        return "";
+    }
+    const formData = new FormData();
+    formData.append("file", audioFile, "audio.wav");
+
+    try {
+        const respose = await axios.post(
+            "https://echo-6sdzv54itq-uc.a.run.app/kateb",
+            formData,
+            { headers: { "Content-Type": "multipart/form-data" } }
+        );
+        const words = respose.data?.json?.words || [];
+        console.log('this is the words ->', words)
+        return words
+    } catch (error) {
+        console.log('>>>>>>>>>>>', error)
+    }
+
 
 }
