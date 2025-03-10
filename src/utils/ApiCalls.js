92bdc24f0427f638c3888068009efb7a53bcffb4
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-03-10 04:48:19 +0200

    addng the hilighted text

--- src/utils/ApiCalls.js
+++ src/utils/ApiCalls.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const sendToKatib = async (audioFile) => {
+
     if (!audioFile) {
         alert("Fetch Error: there is No Audio Selected");
         return "";
@@ -16,7 +17,6 @@ export const sendToKatib = async (audioFile) => {
         );
         const words = respose.data?.json?.words || [];
         const text = words.map(word => word.text).join(" ");
-        console.log(text);
 
         return text
 
@@ -24,5 +24,48 @@ export const sendToKatib = async (audioFile) => {
         console.log('failed to fetch words =>>', error)
     }
 
+}
+
+export const sendToNatiq = async (text) => {
+
+    if (!text.trim()) {
+        alert("there is No Text");
+        return { audioURL: null, wordTimings: [] };
+    }
+
+
+    try {
+        const response = await axios.post('https://echo-6sdzv54itq-uc.a.run.app/natiq',
+            { text },
+            { headers: { "Content-Type": "application/json" } }
+        );
+        const waveAudio = response.data?.wave;
+        const durations = response.data?.durations || [];
+        if (!waveAudio) return { audioURL: null, wordTimings: [], error: "second" };
+
+
+        // remove the decoded data
+        // هنا بنبدل  ال "-" بال "+" وكمان "_" بال "/" تمام
+        const normalBase64 = waveAudio.replace(/-/g, "+").replace(/_/g, "/");
+        // this transfer  from "dhasjdjasj" to "welcome" 
+        const normalText = atob(normalBase64);
+        // in this  by  using Unit8Arry we trnsfer  int  array of number  to  store  the  data as number
+        const blob = new Blob([Uint8Array.from(normalText, c => c.charCodeAt(0))], { type: "audio/wav" });
+        const url = URL.createObjectURL(blob);
+        const wordTimings = durations.map(([word, start, end]) => {
+            return {
+                text: text,
+                start: start * 1000,
+                end: end * 1000
+            }
+        })
+
+        return { url, wordTimings }
+
+    } catch (error) {
+
+        console.log(Error);
+
+    }
 
 }
